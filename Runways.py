4e8918d5285a27ce9723b9303dce8d7190cd8731
eveevans340 <eveevans@riccarton.school.nz>
2025-03-25 09:40:29 +1300

    Fixing the runway number calculation + spacing the code to make it more readable

--- Runways.py
+++ Runways.py
@@ -1,7 +1,7 @@
 '''This program asks for an angle and then calculates the runway number of that angle, it then prints out each runway number in order'''
 
 # Creates list for the runway numbers
-runway_numbers = []
+runway_list = []
 # Minimum angle
 MIN_ANGLE = 0
 # Maximum angle
@@ -11,25 +11,31 @@ MAX_ANGLE = 359
 while True:
     try:
         angle = (input('Enter your angle please: '))
+
         # Condition to break the loop
         if angle == 'complete':
             break
-        else:
-         # Converting the angle into an integer
-         angle = int(angle)
-         # Minimum and Maximum number for program to run
+
+        # Converting the angle into an integer
+        angle = int(angle)
+
+        # Minimum and Maximum number for program to run
         if angle < MIN_ANGLE or angle > MAX_ANGLE:
             print('ERROR: INVALID ANGLE')
-        # Calculation for the runway number
-        runway_calculation = angle + 4 / 10
-        # Changing the runway number to 0 if runway_calculation equals 36
-        if runway_calculation == 36:
-            runway_calculation = 0
-        # Adding the runway number onto the list
-        runway_numbers.append(runway_calculation)
-    except ValueError:
-        print('ERROR: INVALID ANGLE')
 
+        # Calculation for the runway number
+        runway_number = (angle + 4 ) / 10
+        runway_rounded = int(round(runway_number, 0))
+
+        # Changing the runway number to 0 if runway_calculation equals 36
+        if runway_number == 36:
+            runway_number = 0
+
+        # Adding the runway number onto the list
+        runway_list.append(runway_rounded)
+        
+ except ValueError:
+        print('ERROR: INVALID ANGLE')
 # Printing all of the runway numbers
-for number in runway_numbers:
+for number in runway_list:
     print(number)
