af85938f4b82298e9fc690c2a24f5f461d5af743
eveevans340 <eveevans@riccarton.school.nz>
2025-03-25 08:54:26 +1300

    Making a list + loop for my code

--- Runways.py
+++ Runways.py
@@ -1,2 +1,18 @@
 '''This program asks for an angle and then calculates the runway number of that angle, it then prints out each runway number in order'''
 
+# Creates list for the runway numbers
+runway_numbers = []
+
+# Calculation for the runway numbers
+runway_calculation = angle + 4 / 10
+
+# Asking for an angle
+while True:
+    angle = (input('Enter your angle please:'))
+    # Breaking the loop if 'complete' is entered
+    if angle == 'complete':
+        break
+    # Converting the angle into an integer
+    angle = int(angle)
+    if runway_calculation == 36:
+        runway_calculation = 0
