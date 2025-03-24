e2ef5f768f5d0898642394aa412fb1eb6c17776c
eveevans340 <eveevans@riccarton.school.nz>
2025-03-25 08:57:09 +1300

    Adding the runway calculation and adding the runway numbers onto the list

--- Runways.py
+++ Runways.py
@@ -3,9 +3,6 @@
 # Creates list for the runway numbers
 runway_numbers = []
 
-# Calculation for the runway numbers
-runway_calculation = angle + 4 / 10
-
 # Asking for an angle
 while True:
     angle = (input('Enter your angle please:'))
@@ -14,5 +11,9 @@ while True:
         break
     # Converting the angle into an integer
     angle = int(angle)
+    # Calculation for the runway number
+    runway_calculation = angle + 4 / 10
     if runway_calculation == 36:
         runway_calculation = 0
+    # Adding the runway number onto the list
+    runway_numbers.append(angle)
